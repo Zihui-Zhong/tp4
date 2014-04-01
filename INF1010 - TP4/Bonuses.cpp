52fa8d974c8abe9556486f3efd468cc089f3bda4
Zigui <zihui_zhong@hotmail.com>
2014-03-31 21:15:15 -0400

    Bonuses 2nd
    All bonus implemented

--- INF1010 - TP4/Bonuses.cpp
+++ INF1010 - TP4/Bonuses.cpp
@@ -8,12 +8,27 @@
 
 using namespace std;
 
-	SkillPoints<int> MagicWeaponEffect::compute ( const SkillPoints<int>& sp) const{
-		SkillPoints<int> skillBonus(sp.fighting/4+15,0,0);
-		return skillBonus;
+SkillPoints<int> MagicWeaponEffect::compute ( const SkillPoints<int>& sp) const{
+	SkillPoints<int> skillBonus(sp.fighting/4+15,0,0);
+	return skillBonus;
 
 
-	}
+}
 
 
+
+SkillPoints<int> MagicBowEffect::compute ( const SkillPoints<int>& ) const{
+	SkillPoints<int> skillBonus(0,sp.marksmanship/4+15,0);
+	return skillBonus;
+}
+
+SkillPoints<int> MagicWandEffect::compute ( const SkillPoints<int>& ) const{
+	SkillPoints<int> skillBonus(0,0,sp.sorcery/4+15,0);
+	return skillBonus;
+}
+
+SkillPoints<int> SkillPotionEffectcompute ( const SkillPoints<int>& ) const{
+	SkillPoints<int> skillBonus(sp.fighting/10+20,sp.marksmanship/10+20,sp.sorcery/10+20);
+	return skillBonus;
+}
 
